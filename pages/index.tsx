a563a94419f671e395b67025921243f113f3be37
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-02-20 20:03:55 -0800

    finished section 6

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import play3 from '../playground-section-3';
 import play4 from '../playground-section-4';
 import play5 from '../playground-section-5';
+import play6 from '../playground-section-6';
 
 import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    play5();
+    play6();
   }, []);
 
   return <div>Hello World</div>;
