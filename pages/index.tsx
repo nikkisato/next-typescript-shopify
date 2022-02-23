6c0dbd6498a63d986779428868da6d21e878b376
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-02-23 08:59:24 -0800

    finished sectioni 9

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
-import play3 from '../playground-section-3';
-import play4 from '../playground-section-4';
-import play5 from '../playground-section-5';
-import play6 from '../playground-section-6';
-import play7 from '../playground-section-7';
-import play8 from '../playground-section-8';
+import type { InferGetStaticPropsType } from 'next';
+import getAllProducts from "../framework/shopify/product/get-all-products";
 
-import { useEffect } from 'react';
+export async function getStaticProps() {
+const products = await getAllProducts()
 
-export default function Home() {
-  useEffect(() => {
-    play8();
-  }, []);
+  return { 
+    props: {
+      products
+    },
+    revalidate: 4 * 60 * 60
+  }
+}
 
-  return <div>Hello World</div>;
+export default function Home({
+  products
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+
+  return <div>{JSON.stringify(products)}</div>;
 }
