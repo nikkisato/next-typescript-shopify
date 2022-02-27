2126fa81690fdbae2db598890f33300fb08454f5
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-02-26 22:23:29 -0800

    finished section 15

--- framework/common/types/product.ts
+++ framework/common/types/product.ts
@@ -1,15 +1,19 @@
 export interface ProductImage {
-    url: string;
-    alt?: string;
-
-}
-
-export interface Product {
-    id: string;
-    name: string;
-    vendor: string;
-    description: string;
-    path: string;
-    slug: string;
-    images: ProductImage[];
-}
+    url: string
+    alt?: string
+  }
+  
+  export interface ProductPrice {
+    value: number
+    currencyCode: "USD" | "EUR" | string
+  }
+  
+  export interface Product {
+    id: string
+    name: string
+    description: string
+    slug: string
+    path: string
+    images: ProductImage[]
+    price: ProductPrice
+  }
--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -1,40 +1,46 @@
-
 import {
-    ImageEdge,
-    Product as ShopifyProduct
-  } from "../schema"
-  
-  import { Product } from "@common/types/product"
-  
-  const normalizeProductImages = ({edges}: {edges: Array<ImageEdge>}) =>
-    edges.map(({node: { originalSrc: url, ...rest}}) => ({
-        url: `/images/${url}`,
-        ...rest }
-    ))
-  
-  
-  export function normalizeProduct(productNode: ShopifyProduct): Product {
-    const {
-      id,
-      title: name,
-      handle,
-      vendor,
-      description,
-      images: imageConnection,
-      ...rest
-    } = productNode
-  
-    const product = {
-      id,
-      name,
-      vendor,
-      description,
-      path: `/${handle}`,
-      slug: handle.replace(/^\/+|\/+$/g, ""),
-      images: normalizeProductImages(imageConnection),
-      ...rest
-    }
-  
-    return product
+  ImageEdge,
+  MoneyV2,
+  Product as ShopifyProduct
+} from "../schema"
+
+import { Product } from "@common/types/product"
+
+const normalizeProductImages = ({edges}: {edges: Array<ImageEdge>}) =>
+  edges.map(({node: { originalSrc: url, ...rest}}) => ({
+      url: `/images/${url}`,
+      ...rest }
+  ))
+
+const normalizeProductPrice = ({currencyCode, amount}: MoneyV2) => ({
+  value: +amount,
+  currencyCode
+})
+
+
+export function normalizeProduct(productNode: ShopifyProduct): Product {
+  const {
+    id,
+    title: name,
+    handle,
+    vendor,
+    description,
+    images: imageConnection,
+    priceRange,
+    ...rest
+  } = productNode
+
+  const product = {
+    id,
+    name,
+    vendor,
+    description,
+    path: `/${handle}`,
+    slug: handle.replace(/^\/+|\/+$/g, ""),
+    images: normalizeProductImages(imageConnection),
+    price: normalizeProductPrice(priceRange.minVariantPrice),
+    ...rest
   }
-  
+
+  return product
+}
