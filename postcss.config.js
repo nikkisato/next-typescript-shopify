c787ab5c18110994673bb3e4f321d5a67a2e28f2
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-02-26 21:35:34 -0800

    Reverting

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,7 @@
 module.exports = {
   plugins: [
-    'postcss-import',
-    'tailwindcss',
-    'postcss-flexbugs-fixes',
-    'postcss-nested',
-    'postcss-css-variables',
-    [
-      'postcss-preset-env',
-      {
-        "autoprefixer": {
-          "flexbox": 'no-2009',
-        },
-        "stage": 0,
-        "features": {
-          'custom-properties': false,
-          'nesting-rules': true,
-        },
-      },
-    ],
-  ],
+    "tailwindcss",
+    "postcss-nesting",
+    "autoprefixer"
+  ]
 }
