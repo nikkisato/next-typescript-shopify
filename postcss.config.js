943edecf37c329f6897b96a12636aacf30c35272
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-02-26 21:26:44 -0800

    finished section 14

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,22 @@
 module.exports = {
-    plugins: [
-      "tailwindcss",
-      "autoprefixer"
-    ]
-  }
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    'postcss-nested',
+    'postcss-css-variables',
+    [
+      'postcss-preset-env',
+      {
+        "autoprefixer": {
+          "flexbox": 'no-2009',
+        },
+        "stage": 0,
+        "features": {
+          'custom-properties': false,
+          'nesting-rules': true,
+        },
+      },
+    ],
+  ],
+}
