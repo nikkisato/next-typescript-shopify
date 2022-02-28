7b2ffffbdcd70be1cfabe015ffbca141f12739c4
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-02-27 20:09:40 -0800

    finished section 17

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,12 +1,15 @@
 import { FunctionComponent } from "react"
 import s from "./Layout.module.css"
+import { Footer, Navbar } from "@components/common"
 
 const Layout: FunctionComponent = ({children}) => {
   return (
     <div className={s.root}>
+        <Navbar />
       <main className="fit">
     { children }
     </main>
+    <Footer />
   </div>
   )
 }
