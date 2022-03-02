a4fafc017b11d3ed0111f0164338d28d5848a3c9
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-03-01 19:03:54 -0800

    finished section 19

--- framework/shopify/utils/fetch-api.ts
+++ framework/shopify/utils/fetch-api.ts
@@ -1,29 +1,32 @@
 import {
-    ApiFetcherOptions,
-    ApiFetcherResults
-  } from "@common/types/api"
-  
-  const fetchApi = async <T>({
-    url,
-    query }: ApiFetcherOptions
-  ): Promise<ApiFetcherResults<T>> => {
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        query
-      })
+  ApiFetcherOptions,
+  ApiFetcherResults
+} from "@common/types/api"
+
+const fetchApi = async <T>({
+  url,
+  query,
+  variables }: ApiFetcherOptions
+): Promise<ApiFetcherResults<T>> => {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      query,
+      variables
     })
-  
-    const { data, errors} = await res.json()
-    if (errors) {
-      throw new Error(errors[0].message ?? errors.message)
-    }
-  
-    return { data }
+  })
+
+  const { data, errors} = await res.json()
+  // ?? is checking if left hand expression is null or undefined -> if it is go with right expression
+  // || is checking if left hand expression is null, undefined, "", 0, false
+  if (errors) {
+    throw new Error(errors[0].message ?? errors.message)
   }
-  
-  export default fetchApi
-  
+
+  return { data }
+}
+
+export default fetchApi
