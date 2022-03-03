fe29645463fce26a9cc78e0c82dc055ced6b2166
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-03-02 18:15:46 -0800

    finished section 22

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -1,17 +1,36 @@
 import cn from 'classnames'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import s from './ProductView.module.css'
 import { Container, Button } from '@components/ui'
 import Image from "next/image"
 import { Product } from '@common/types/product'
-import { ProductSlider } from "@components/product"
+import { ProductSlider, Swatch } from "@components/product"
+import { Choices, getVariant } from '../helpers'
+import { useUI } from '@components/ui/context'
 
 interface Props {
   product: Product
 }
 
-const ProductView: FC<Props> = ({ product }) => {
 
+const ProductView: FC<Props> = ({ product }) => {
+  const [ choices, setChoices ] = useState<Choices>({})
+  const { openSidebar } = useUI()
+
+  const variant = getVariant(product, choices)
+
+  const addToCart = () => {
+    try {
+      const item = {
+        productId: String(product.id),
+        variantId: variant?.id,
+        variantOptions: variant?.options
+      }
+
+      alert(JSON.stringify(item))
+      openSidebar()
+    } catch {}
+  }
   return (
     <Container>
       <div className={cn(s.root, 'fit', "mb-5")}>
@@ -43,21 +62,39 @@ const ProductView: FC<Props> = ({ product }) => {
         </div>
         <div className={s.sidebar}>
           <section>
-            <div className="pb-4">
-              <h2 className="uppercase font-medium">Color</h2>
-              <div className="flex flex-row py-4">
-                Variant Options Here!
+            { product.options.map(option =>
+              <div key={option.id} className="pb-4">
+                <h2 className="uppercase font-medium">{option.displayName}</h2>
+                <div className="flex flex-row py-4">
+                { option.values.map(optValue => {
+                    const activeChoice = choices[option.displayName.toLowerCase()]
+                    return (
+                      <Swatch
+                        key={`${option.id}-${optValue.label}`}
+                        label={optValue.label}
+                        color={optValue.hexColor}
+                        variant={option.displayName}
+                        active={optValue.label.toLowerCase() === activeChoice}
+                        onClick={() => {
+                          setChoices({
+                            ...choices,
+                            [option.displayName.toLowerCase()]: optValue.label.toLowerCase()
+                          })
+                        }}
+                      />
+                    )}
+                  )}
+                </div>
               </div>
-            </div>
+            )}
             <div className="pb-14 break-words w-full max-w-xl text-lg">
               { product.description }
             </div>
           </section>
           <div>
-          <Button
+            <Button
               className={s.button}
-              onClick={() => alert("adding to cart")}>
-              Add to Cart
+              onClick={addToCart}>              Add to Cart
             </Button>
           </div>
         </div>
