c7db71287a45dc91109891fb6bdfda745b73813b
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-03-07 20:21:28 -0800

    fixed file

--- framework/common/types/api.ts
+++ framework/common/types/api.ts
@@ -1,26 +1,28 @@
-import { ApiFetcher, ApiFetcherOptions } from "./api"
+import { ApiHooks } from "./hooks"
 
-export interface ApiHooks {
-  cart: {
-    useAddItem: MutationHook
-    useCart: any
-  }
+export type ApiFetcherOptions = {
+  query: string
+  variables?: Variables
 }
 
-export type MutationHookContext = {
-  fetch: (input: any) => any
+export type Variables = {[key: string]: string | any | undefined}
+
+export type ApiFetcherResults<T> = {
+  data: T
 }
 
-export type FetcherHookContext = {
-  input?: any
-  fetch: ApiFetcher
+export interface ApiConfig {
+  fetch<T>(options: ApiFetcherOptions): Promise<ApiFetcherResults<T>>
+  checkoutCookie: string
+}
+
+
+export type ApiFetcher<T = any> = (
   options: ApiFetcherOptions
-}
+) => Promise<ApiFetcherResults<T>>
 
-export type MutationHook = {
-  fetcherOptions: ApiFetcherOptions
-  fetcher: (context: FetcherHookContext) => any
-  useHook(
-    context: MutationHookContext
-  ): (input: any) => any
+export interface ApiProviderContext {
+  hooks: ApiHooks
+  fetcher: ApiFetcher
+  checkoutCookie: string
 }
